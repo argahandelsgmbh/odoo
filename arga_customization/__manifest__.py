2232a502f0dfc27c5358fc50e84605e9a1df03f0
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2023-04-12 13:22:07 +0500

    Update __manifest__.py

--- arga_customization/__manifest__.py
+++ arga_customization/__manifest__.py
@@ -18,6 +18,7 @@
     # for the full list
     'category': 'sale',
     'version': '0.1',
+    'license': 'OPL-1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale', 'stock', 'calendar', 'account', 'purchase','exim_bellona','istikbal'],
