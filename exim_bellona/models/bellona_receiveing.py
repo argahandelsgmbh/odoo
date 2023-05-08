202e2bf3aa6203239c1b752459f33c5aea6cfa90
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2023-05-08 18:21:24 +0500

    Add files via upload

--- exim_bellona/models/bellona_receiveing.py
+++ exim_bellona/models/bellona_receiveing.py
@@ -87,3 +87,49 @@ class BellonaReceive(models.Model):
                         if stock_picking.state == 'done':
                             self.picking_id = pick_id
                             self.is_received = True
+
+    def receive_all_products(self):
+        for rec in self:
+            rec.purchase_id = self.env['purchase.order'].search([("name", '=', rec.po_no)], limit=1)
+            if rec.purchase_id.state == 'purchase' and not rec.is_received:
+                lines = rec.purchase_id.order_line.filtered(lambda i: i.product_id.default_code == rec.internal_ref)
+                if lines:
+                    if not rec.picking_id:
+                        pick = lines.move_ids.filtered(
+                            lambda h: h.product_id.default_code == rec.internal_ref).picking_id.ids
+                        pick_id = pick[-2] if len(pick) > 1 else pick[0]
+                        if rec.picking_id.state == 'done':
+                            rec.picking_id = pick_id
+                            rec.is_received = True
+                    for move in lines.move_ids:
+                        if move.state not in ['done', 'cancel']:
+                            move.quantity_done = rec.quantity
+                    if len(lines.move_ids) > 1:
+                        action_data = lines.move_ids.filtered(
+                            lambda h: h.state not in ['done', 'cancel']).picking_id.with_context(
+                            skip_backorder=False).button_validate()
+                        backorder_wizard = self.env['stock.backorder.confirmation'].with_context(action_data['context'])
+                        backorder_wizard.process()
+                        if not rec.picking_id:
+                            pick = lines.move_ids.filtered(
+                                lambda h: h.product_id.default_code == rec.internal_ref).picking_id.ids
+                            pick_id = pick[-2] if len(pick) > 1 else pick[0]
+                            stock_picking = self.env['stock.picking'].browse([pick_id])
+                            if stock_picking.state == 'done':
+                                rec.picking_id = pick_id
+                                rec.is_received = True
+                    else:
+                        action_data = lines.move_ids.filtered(
+                            lambda h: h.state not in ['done', 'cancel']).picking_id.with_context(
+                            skip_backorder=False).button_validate()
+                        if 'context' in str(action_data):
+                            backorder_wizard = self.env['stock.backorder.confirmation'].with_context(action_data['context'])
+                            backorder_wizard.process()
+                        if not rec.picking_id:
+                            pick = lines.move_ids.filtered(
+                                lambda h: h.product_id.default_code == rec.internal_ref).picking_id.ids
+                            pick_id = pick[-2] if len(pick) > 1 else pick[0]
+                            stock_picking = self.env['stock.picking'].browse([pick_id])
+                            if stock_picking.state == 'done':
+                                rec.picking_id = pick_id
+                                rec.is_received = True
