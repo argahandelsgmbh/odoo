ed9f31d512de16f0247f39f8abcdfa74c767f497
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2023-07-02 00:19:09 +0500

    Add files via upload

---
+++ istikbal/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from . import istikbal_settings
+from . import product_template
+from . import res_config_settings
+from . import shipment
+from . import istikbal_schedulers
+from . import istikbal_log_notes
+from . import combine_shipments
