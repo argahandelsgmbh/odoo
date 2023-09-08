4e037c623affd7966e8004a04a10eb426e255ad9
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2023-09-08 11:48:59 +0500

    Update __manifest__.py

--- helpdesk_to_po/__manifest__.py
+++ helpdesk_to_po/__manifest__.py
@@ -18,6 +18,7 @@
     # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
+     'license': 'OPL-1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'helpdesk', 'purchase'],
