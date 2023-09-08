18e2c64286338da8941f7f5150af9d596aca2535
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2023-09-08 12:14:44 +0500

    Update models.py

--- arga_customization/models/models.py
+++ arga_customization/models/models.py
@@ -361,7 +361,7 @@ class CalendarEvent(models.Model):
 class PurchaseOrderInh(models.Model):
     _inherit = 'purchase.order'
 
-    sale_order = fields.Many2one('sale.order', compute='_compute_sale_order',store=True)
+    sale_order = fields.Many2one('sale.order', compute='_compute_sale_order')
     sale_repair_id = fields.Many2one('sale.order')
     receipt_status = fields.Selection(selection=[
         ('draft', 'Draft'), ('waiting', 'Waiting for another Operations'),
