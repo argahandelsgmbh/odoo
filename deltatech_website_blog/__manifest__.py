233b5e1ff352e302e6bcf0ecac43aa31cefc3fcd
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2023-10-07 14:51:00 +0500

    Add files via upload

---
+++ deltatech_website_blog/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+{
+    "name": "Deltatech Web Site Blog",
+    "category": "Website",
+    "summary": "Blog Extension",
+    "version": "15.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "license": "OPL-1",
+    "website": "https://www.terrabit.ro",
+    "depends": ["website_blog"],
+    "data": [],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
