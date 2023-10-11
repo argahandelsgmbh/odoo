10065714f343b423b931f2dcb21a86eec45a4b2d
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2023-10-11 10:59:25 +0500

    Add files via upload

--- payment_from_so/__manifest__.py
+++ payment_from_so/__manifest__.py
@@ -20,7 +20,7 @@
     'license': 'OPL-1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale', 'stock', 'purchase'],
+    'depends': ['base', 'sale', 'stock', 'purchase', 'sale_stock'],
 
     # always loaded
     'data': [
--- payment_from_so/models/models.py
+++ payment_from_so/models/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import json
 
 from odoo import models, fields, api
 
@@ -14,33 +15,65 @@ class PurchaseOrder(models.Model):
                 order.compute_is_po_draft()
         return rec
 
+    sale_ids = fields.Many2many('sale.order', compute="compute_sales")
+
+    @api.depends('name')
+    def compute_sales(self):
+        print(self.env['sale.order'].search([('origin', '=', self.name)]).ids)
+        sale_order_ids = self._get_sale_orders().ids
+        self.sale_ids = sale_order_ids
+
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-
 
     payment_count = fields.Integer(compute='count_payments')
     date_order = fields.Datetime(string='Order Date', required=True, readonly=False,  index=True, copy=False, default=fields.Datetime.now, help="Creation date of draft/sent orders,\nConfirmation date of confirmed orders.")
     payment_ids = fields.Many2many('account.payment', compute="compute_payments")
     purchase_order_ids = fields.Many2many('purchase.order', compute="compute_purchases")
+    commitment_date = fields.Datetime('Delivery Date', copy=False,
+                                      help="This is the delivery date promised to the customer. "
+                                           "If set, the delivery order will be scheduled based on "
+                                           "this date rather than product lead times.")
+
 
     @api.depends('name')
     def compute_purchases(self):
         self.purchase_order_ids = self.env['purchase.order'].search([('origin', '=', self.name)]).ids
 
     def compute_payments(self):
-        self.payment_ids = self.env['account.payment'].search([('ref', '=', self.name)]).ids
+        pay_list = []
+        for inv in self.invoice_ids:
+            reconciled_payments_widget_vals = json.loads(inv.invoice_payments_widget)
+
+            if inv.invoice_payments_widget != 'false':
+                pay_list += [vals['account_payment_id'] for vals in reconciled_payments_widget_vals['content']]
+
+        payment = self.env['account.payment'].search([('ref', '=', self.name)]).ids
+        self.payment_ids = payment + pay_list
 
     def count_payments(self):
         for rec in self:
-            rec.payment_count = self.env['account.payment'].search_count([('ref', '=', self.name)])
+            pay_list = []
+            for inv in rec.invoice_ids:
+                reconciled_payments_widget_vals = json.loads(inv.invoice_payments_widget)
+
+                if inv.invoice_payments_widget != 'false':
+                    pay_list += [vals['account_payment_id'] for vals in reconciled_payments_widget_vals['content']]
+            rec.payment_count = self.env['account.payment'].search_count([('ref', '=', rec.name)]) + len(pay_list)
 
     def action_show_payments(self):
         self.ensure_one()
+        pay_list = []
+        for inv in self.invoice_ids:
+            reconciled_payments_widget_vals = json.loads(inv.invoice_payments_widget)
+
+            if inv.invoice_payments_widget != 'false':
+                pay_list += [vals['account_payment_id'] for vals in reconciled_payments_widget_vals['content']]
         return {
             'name': 'Payments',
             'res_model': 'account.payment',
-            'domain': [('ref', '=', self.name)],
+            'domain': ['|', ('ref', '=', self.name), ('id', 'in', pay_list)],
             'view_mode': 'tree,form',
             'type': 'ir.actions.act_window',
             'context': "{'create': False}"
