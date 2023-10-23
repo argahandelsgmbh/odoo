0bea7aa5dba795b4805a496498d4309f4fe68a41
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2023-10-23 14:18:47 +0500

    Update shipment.py

--- istikbal/models/shipment.py
+++ istikbal/models/shipment.py
@@ -93,7 +93,7 @@ class ShipmentDetails(models.Model):
     company_id = fields.Many2one('res.company', string='Company', required=True, readonly=True,
                                  default=lambda self: self.env.company)
 
-    qr_image = fields.Binary("QR Code")
+    qr_image = fields.Binary("QR Code",compute="_generate_qr_code")
     purchase_id = fields.Many2one('purchase.order')
     sale_id = fields.Many2one('sale.order')
     combine_id = fields.Many2one('istikbal.combine.shipments')
@@ -101,6 +101,23 @@ class ShipmentDetails(models.Model):
     price = fields.Float()
     picking_id = fields.Many2one('stock.picking')
     subtotal = fields.Float()
+
+    
+    def _generate_qr_code(self):
+        for i in self:
+            qr = qrcode.QRCode(
+                version=1,
+                error_correction=qrcode.constants.ERROR_CORRECT_L,
+                box_size=20,
+                border=4,
+            )
+            qr.add_data(i.productCode)
+            qr.make(fit=True)
+            img = qr.make_image()
+            temp = BytesIO()
+            img.save(temp, format="PNG")
+            qr_img = base64.b64encode(temp.getvalue())
+            i.qr_image = qr_img
 
 
 
