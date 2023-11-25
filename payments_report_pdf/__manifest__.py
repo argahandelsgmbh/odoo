a2817a0cc3a8a49467ace0492518207aea058729
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2023-11-25 20:46:34 +0500

    Add files via upload

---
+++ payments_report_pdf/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Payments Report",
+
+    'summary': """
+        Payments Report""",
+
+    'description': """
+        Payments Report
+    """,
+
+    'author': "HAKTechnologies.com",
+    'website': "http://HAKTechnologies.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.2',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'account'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/payment_report_wizard.xml',
+        'report/payment_report.xml',
+    ],
+
+}
