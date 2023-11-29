28addaeb2157ddf46c8bbdf662d143f6d73c6111
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2023-11-29 16:12:42 +0500

    Update __manifest__.py

--- invoices_report_pdf/__manifest__.py
+++ invoices_report_pdf/__manifest__.py
@@ -9,8 +9,8 @@
         Invoices Report
     """,
 
-    'author': "Atif Ali",
-    'website': "http://www.yourcompany.com",
+    'author': "Haktechnologies",
+    'website': "http://www.haktechnologies.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
