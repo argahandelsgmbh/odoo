b7225be980323b13205144425c212a9743e7033e
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2024-01-02 18:33:58 +0500

    Update pricelists.py

--- hak_products_var_import/models/pricelists.py
+++ hak_products_var_import/models/pricelists.py
@@ -30,7 +30,7 @@ class ProductVarImport(models.Model):
                         _logger.info('Assigned %s price code to %s product', pcount, rec.pricecode)
 
     def cron_import_products(self):
-        for rec in self.env['pricelist.pricelist'].search(["imp", '=', False):
+        for rec in self.env['pricelist.pricelist'].search([("imp", '=', False)], order='id DESC'):
             if rec.pricecode and rec.imp == False:
                 l = len(rec.pricecode)
                 products = self.env['product.template'].search(['|',("default_code", 'ilike', rec.pricecode),("pricecode", '=', rec.pricecode)]).filtered(lambda o:o.default_code[:l] == rec.pricecode)
