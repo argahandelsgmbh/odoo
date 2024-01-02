66670ec7480ac3e51f848a063474ddc84573d1c9
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2024-01-02 14:09:37 +0500

    Update var_wizard.py

--- hak_products_var_import/models/var_wizard.py
+++ hak_products_var_import/models/var_wizard.py
@@ -36,26 +36,26 @@ class ProductVarImport(models.TransientModel):
                 data.append(elm)
             _logger.info('Length %s', len(data))
             count = 0
-        for rec in data:
-            if rec.get('pricecode'):
-                products = self.env['product.template'].search([("default_code", 'ilike', rec.get('pricecode')), ("standard_price", '=', False)])
-                for p in products:
-                    l = len(rec.get('pricecode'))
-                    if p.default_code[:l] == rec.get('pricecode'):
-                        factor = self.env['product.category'].search([("name", '=', rec.get('category'))],
-                                                                     limit=1).factor
-                        
-                        p.price_code = rec.get('pricecode')
-                        p.standard_price = rec.get('cost')
-                        p.list_price = rec.get('cost') * (factor or 1)
-                        count = count + 1
-                        pcount = pcount + 1
-                        if count == 250:
-                            _logger.info('Assigned %s price code to %s product', pcount, rec.get('pricecode'))
-                            self._cr.commit()
-                            count = 0
-        _logger.info('All done %s ', pcount)
-        return {'type': 'ir.actions.client', 'tag': 'reload'}
+            for rec in data:
+                if rec.get('pricecode'):
+                    products = self.env['product.template'].search([("default_code", 'ilike', rec.get('pricecode')), ("standard_price", '=', False)])
+                    for p in products:
+                        l = len(rec.get('pricecode'))
+                        if p.default_code[:l] == rec.get('pricecode'):
+                            factor = self.env['product.category'].search([("name", '=', rec.get('category'))],
+                                                                         limit=1).factor
+                            
+                            p.price_code = rec.get('pricecode')
+                            p.standard_price = rec.get('cost')
+                            p.list_price = rec.get('cost') * (factor or 1)
+                            count = count + 1
+                            pcount = pcount + 1
+                            if count == 250:
+                                _logger.info('Assigned %s price code to %s product', pcount, rec.get('pricecode'))
+                                self._cr.commit()
+                                count = 0
+            _logger.info('All done %s ', pcount)
+            return {'type': 'ir.actions.client', 'tag': 'reload'}
 
 
 class ProductCategories(models.Model):
