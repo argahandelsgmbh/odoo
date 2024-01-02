eb6ffd6e3c761a629f2d4485421a1259081da99f
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2024-01-02 13:37:48 +0500

    Update var_wizard.py

--- hak_products_var_import/models/var_wizard.py
+++ hak_products_var_import/models/var_wizard.py
@@ -4,7 +4,9 @@ import logging
 from odoo import models, fields, api
 from odoo.exceptions import UserError
 from datetime import datetime
+
 _logger = logging.getLogger(__name__)
+
 
 class ProductTemplateInh(models.Model):
     _inherit = 'product.template'
@@ -19,7 +21,7 @@ class ProductVarImport(models.TransientModel):
 
     def action_import_products(self):
         wb = xlrd.open_workbook(file_contents=base64.decodebytes(self.file_upload))
-        pcount=0
+        pcount = 0
         for s in wb.sheets():
             first_row = []  # Header
             for col in range(s.ncols):
@@ -31,32 +33,31 @@ class ProductVarImport(models.TransientModel):
                     elm[first_row[col]] = s.cell_value(row, col)
                 data.append(elm)
             _logger.info('Length %s', len(data))
-            count=0
-           
-            for rec in data:
-                if rec.get('pricecode'):
-                    _logger.info('Assigned %s price code to %s product',  pcount, rec.get('pricecode'))
-                    products = self.env['product.template'].search([("default_code", 'ilike', rec.get('pricecode')),("standard_price", '=',False)])
-                    for p in products:
-                        l=len(rec.get('pricecode'))
-                        if p.default_code[:l]==rec.get('pricecode'):
-                           factor = self.env['product.category'].search([("name", '=', rec.get('category'))],
-                                                                        limit=1).factor
-                           _logger.info('Assigned %s price code to %s product',  pcount, rec.get('pricecode'))
-                           p.price_code = rec.get('pricecode')
-                           p.standard_price = rec.get('cost')
-                           p.list_price=rec.get('cost')*(factor or 1)
-                           count=count+1 
-                           pcount=pcount+1
-                           if count==250: 
-                               self._cr.commit()
-                               count=0
-        _logger.info('All done %s ',pcount)
+            count = 0
+        for rec in data:
+            if rec.get('pricecode'):
+                _logger.info('Assigned %s price code to %s product', pcount, rec.get('pricecode'))
+                products = self.env['product.template'].search(
+                    [("default_code", 'ilike', rec.get('pricecode')), ("standard_price", '=', False)])
+                for p in products:
+                    l = len(rec.get('pricecode'))
+                    if p.default_code[:l] == rec.get('pricecode'):
+                        factor = self.env['product.category'].search([("name", '=', rec.get('category'))],
+                                                                     limit=1).factor
+                        _logger.info('Assigned %s price code to %s product', pcount, rec.get('pricecode'))
+                        p.price_code = rec.get('pricecode')
+                        p.standard_price = rec.get('cost')
+                        p.list_price = rec.get('cost') * (factor or 1)
+                        count = count + 1
+                        pcount = pcount + 1
+                        if count == 250:
+                            self._cr.commit()
+                            count = 0
+        _logger.info('All done %s ', pcount)
         return {'type': 'ir.actions.client', 'tag': 'reload'}
-
 
 
 class ProductCategories(models.Model):
     _inherit = 'product.category'
 
-    factor=fields.Float("Factor")
+    factor = fields.Float("Factor")
