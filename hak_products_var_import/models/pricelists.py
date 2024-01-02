80ff7caa6f5244dfa2382db5222ee16ecc033c12
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2024-01-02 19:05:10 +0500

    Update pricelists.py

--- hak_products_var_import/models/pricelists.py
+++ hak_products_var_import/models/pricelists.py
@@ -19,7 +19,7 @@ class ProductVarImport(models.Model):
             pcount = pcount + 1
             if rec.pricecode:
                 l = len(rec.pricecode)
-                products = self.env['product.template'].search(['|',('price_code','=',rec.pricecode),('default_code','in',rec.pricecode)]).filtered(lambda o:o.default_code[:l] == rec.pricecode)
+                products = self.env['product.template'].search(['|',('price_code','=',rec.pricecode),('default_code','ilike',rec.pricecode)]).filtered(lambda o:o.default_code[:l] == rec.pricecode)
                 for p in products:
                     if p.default_code[:l] == rec.pricecode or p.pricecode == rec.pricecode:
                         factor = self.env['product.category'].search([("name", '=', rec.category)], limit=1).factor
@@ -35,7 +35,7 @@ class ProductVarImport(models.Model):
         for rec in self.env['pricelist.pricelist'].search([("imp", '=', False)], order='id DESC'):
             if rec.pricecode and rec.imp == False:
                 l = len(rec.pricecode)
-                products = self.env['product.template'].search(['|',('price_code','=',rec.pricecode),('default_code','in',rec.pricecode)]).filtered(lambda o:o.default_code[:l] == rec.pricecode)
+                products = self.env['product.template'].search(['|',('price_code','=',rec.pricecode),('default_code','ilike',rec.pricecode)]).filtered(lambda o:o.default_code[:l] == rec.pricecode)
                 for p in products:
                     if p.default_code[:l] == rec.pricecode or p.pricecode == rec.pricecode:
                         factor = self.env['product.category'].search([("name", '=', rec.category)], limit=1).factor
