ee4615cb03c77805fbe62dd1a0491a0c5b261307
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2024-01-02 14:01:54 +0500

    Update __manifest__.py

--- daily_invoices_report_pdf/__manifest__.py
+++ daily_invoices_report_pdf/__manifest__.py
@@ -9,14 +9,15 @@
         Invoices Report
     """,
 
-    'author': "Atif Ali",
-    'website': "http://www.yourcompany.com",
+    'author': "HAKTechnologies.com",
+    'website': "http://www.haktechnology.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
+    'license' : 'OPL-1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'account'],
