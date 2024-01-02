1dabbea948191c482de670d20261c120a8e2a270
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2024-01-02 18:58:04 +0500

    Update pricelists.py

--- hak_products_var_import/models/pricelists.py
+++ hak_products_var_import/models/pricelists.py
@@ -35,7 +35,7 @@ class ProductVarImport(models.Model):
         for rec in self.env['pricelist.pricelist'].search([("imp", '=', False)], order='id DESC'):
             if rec.pricecode and rec.imp == False:
                 l = len(rec.pricecode)
-                products = self.env['product.template'].search([]).filtered(lambda o:o.default_code[:l] == rec.pricecode)
+                products = self.env['product.template'].search([('default_code','!=',False)]).filtered(lambda o:o.default_code[:l] == rec.pricecode)
                 for p in products:
                     if p.default_code[:l] == rec.pricecode or p.pricecode == rec.pricecode:
                         factor = self.env['product.category'].search([("name", '=', rec.category)], limit=1).factor
