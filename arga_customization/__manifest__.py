a585c938aa677550c687456b5e197a1d2c4e2a66
Hunain AK <hunainfast@gmail.com>
2024-03-21 21:56:46 +0500

    Update __manifest__.py

--- arga_customization/__manifest__.py
+++ arga_customization/__manifest__.py
@@ -21,7 +21,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'sale',
-    'version': '0.1',
+    'version': '17.1',
     'license': 'OPL-1',
 
     # any module necessary for this one to work correctly
