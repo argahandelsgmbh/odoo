991f4b49fccbf5a044ee101ecb2a427d94c50972
Hunain AK <hunainfast@gmail.com>
2024-03-22 17:39:57 +0500

    Update repair.py

--- bsi_repair_parts_immediate_buying/models/repair.py
+++ bsi_repair_parts_immediate_buying/models/repair.py
@@ -22,38 +22,38 @@ from odoo import api, fields, models, _
 from datetime import date, time
 
 
-class RepairLine(models.Model):
-    _inherit = "repair.line"
+# class RepairLine(models.Model):
+#     _inherit = "repair.line"
 
-    is_purchase_part_show = fields.Boolean(string="Is Purchase Part Show")
+#     is_purchase_part_show = fields.Boolean(string="Is Purchase Part Show")
 
-    def action_Purchase_part(self):
-        self.ensure_one()
-        purchase_order = self.env['purchase.order'].search([(
-            'repair_order_id', '=', self.repair_id.id)])
-        context = {'default_repair_order_id': self.repair_id.id,
-                   'default_order_line': [(0, 0, {"product_id": self.id,
-                                                  "name": self.name,
-                                                  "product_qty": self.product_uom_qty,
-                                                  "price_unit": self.price_unit,
-                                                  "product_uom": self.product_uom.id,
-                                                  "date_planned": date.today()})]}
-        return {
-          'res_model': 'purchase.order',
-          'type': 'ir.actions.act_window',
-          'context': context,
-          'view_mode': 'form',
-          'view_type': 'form',
-          'target': 'new',
-        }
+#     def action_Purchase_part(self):
+#         self.ensure_one()
+#         purchase_order = self.env['purchase.order'].search([(
+#             'repair_order_id', '=', self.repair_id.id)])
+#         context = {'default_repair_order_id': self.repair_id.id,
+#                    'default_order_line': [(0, 0, {"product_id": self.id,
+#                                                   "name": self.name,
+#                                                   "product_qty": self.product_uom_qty,
+#                                                   "price_unit": self.price_unit,
+#                                                   "product_uom": self.product_uom.id,
+#                                                   "date_planned": date.today()})]}
+#         return {
+#           'res_model': 'purchase.order',
+#           'type': 'ir.actions.act_window',
+#           'context': context,
+#           'view_mode': 'form',
+#           'view_type': 'form',
+#           'target': 'new',
+#         }
 
-    @api.onchange('product_id', 'product_uom_qty')
-    def _onchange_product_id(self):
-        if self.product_id:
-            if self.product_uom_qty > self.product_id.qty_available:
-                self.is_purchase_part_show = True
-            else:
-                self.is_purchase_part_show = False
+#     @api.onchange('product_id', 'product_uom_qty')
+#     def _onchange_product_id(self):
+#         if self.product_id:
+#             if self.product_uom_qty > self.product_id.qty_available:
+#                 self.is_purchase_part_show = True
+#             else:
+#                 self.is_purchase_part_show = False
 
 
 class Repair(models.Model):
