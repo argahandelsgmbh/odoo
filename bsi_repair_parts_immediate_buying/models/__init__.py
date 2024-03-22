da571f9d070e0da68960b8484689769b96e6d646
Hunain AK <hunainfast@gmail.com>
2024-03-22 15:38:16 +0500

    Update __init__.py

--- bsi_repair_parts_immediate_buying/models/__init__.py
+++ bsi_repair_parts_immediate_buying/models/__init__.py
@@ -18,5 +18,5 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 #################################################################################
-from . import purchase
-from . import repair
+# from . import purchase
+# from . import repair
