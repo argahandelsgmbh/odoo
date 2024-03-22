bbc184ee4a04ae443a97a439f3217b8df3ff3202
Hunain AK <hunainfast@gmail.com>
2024-03-22 17:01:25 +0500

    Update __manifest__.py

--- stock_report_pdf/__manifest__.py
+++ stock_report_pdf/__manifest__.py
@@ -11,6 +11,7 @@
 
     'author': "Atif Ali",
     'website': "http://www.yourcompany.com",
+    'license': 'OPL-1',
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
