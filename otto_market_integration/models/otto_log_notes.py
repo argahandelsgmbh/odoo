66792c93d83459de493bb86ffae2a2d04dad4216
Hunain AK <hunainfast@gmail.com>
2024-03-23 15:57:00 +0500

    Delete otto_market_integration directory

--- otto_market_integration/models/otto_log_notes.py
+++
@@ -1,9 +0,0 @@
-from odoo import _, api, fields, models
-
-
-class OttoLogNotes(models.Model):
-    _name = 'otto.log.notes'
-    _description = "Otto Log Notes"
-    _rec_name = 'error'
-
-    error = fields.Text('Error')
