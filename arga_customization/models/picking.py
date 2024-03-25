7fd5fe08d3481cbac7b19e2607fa63b43c36b2b8
Hunain AK <hunainfast@gmail.com>
2024-03-25 12:00:21 +0500

    Update picking.py

--- arga_customization/models/picking.py
+++ arga_customization/models/picking.py
@@ -13,10 +13,10 @@ class StockPickingInh(models.Model):
     remaining_amt = fields.Float('Open Amount')
     delivery_date = fields.Date(string='Delivery Date', copy=False)
 
-    def write(self, vals):
-        res = super(StockPickingInh, self).write(vals)
-        if vals.get('delivery_date'):
-            sale_order = self.env['sale.order'].search([("name", '=', self.origin)], limit=1)
-            if sale_order and sale_order.delivery_date != self.delivery_date:
-                sale_order.delivery_date = self.delivery_date
-        return res
+    # def write(self, vals):
+    #     res = super(StockPickingInh, self).write(vals)
+    #     if vals.get('delivery_date'):
+    #         sale_order = self.env['sale.order'].search([("name", '=', self.origin)], limit=1)
+    #         if sale_order and sale_order.delivery_date != self.delivery_date:
+    #             sale_order.delivery_date = self.delivery_date
+    #     return res
