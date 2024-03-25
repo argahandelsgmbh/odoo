322e7f468bf88ccd7de0a3c1d024d212eca18efe
Hunain AK <hunainfast@gmail.com>
2024-03-25 11:59:47 +0500

    Update account.py

--- arga_customization/models/account.py
+++ arga_customization/models/account.py
@@ -7,13 +7,13 @@ from datetime import timedelta
 from odoo.exceptions import UserError
 
 
-class AccountLineInh(models.Model):
-    _inherit = 'account.analytic.line'
+# class AccountLineInh(models.Model):
+#     _inherit = 'account.analytic.line'
 
-    def write(self, vals):
-        res = super(AccountLineInh, self).write(vals)
-        if "date" in vals:
-            planning = self.env['planning.slot'].search([('employee_id', '=', self.employee_id.id)]).filtered(lambda i:i.start_datetime.date() >= self.date and i.end_datetime.date() <= self.date)
-            if not planning:
-                raise UserError('This employee is not available on this slot.')
-        return res
+#     def write(self, vals):
+#         res = super(AccountLineInh, self).write(vals)
+#         if "date" in vals:
+#             planning = self.env['planning.slot'].search([('employee_id', '=', self.employee_id.id)]).filtered(lambda i:i.start_datetime.date() >= self.date and i.end_datetime.date() <= self.date)
+#             if not planning:
+#                 raise UserError('This employee is not available on this slot.')
+#         return res
