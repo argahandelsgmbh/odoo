60d10cdc74e405e327d712a56a405719782138a8
Hunain AK <hunainfast@gmail.com>
2024-03-30 13:13:44 +0500

    Update shipment.py

--- istikbal/models/shipment.py
+++ istikbal/models/shipment.py
@@ -102,7 +102,7 @@ class ShipmentDetails(models.Model):
     picking_id = fields.Many2one('stock.picking')
     subtotal = fields.Float()
     product_id = fields.Many2one("product.template", "Product")
-    pricelist_price = fields.Float("Pricelist Price", related="product_id.list_price")
+    pricelist_price = fields.Float("Pricelist Price")
 
     def _generate_qr_code(self):
         for i in self:
