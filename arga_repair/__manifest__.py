7e37e4f0e85338fc2049b6e9f09748569f07b508
Hunain AK <hunainfast@gmail.com>
2024-04-05 16:15:59 +0500

    Add files via upload

---
+++ arga_repair/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Repair/Helpdesk",
+
+    'summary': """
+       Repair/Helpdesk""",
+
+    'description': """
+        Create Delivery from Repair
+        Create PO from Repair
+        Create Repair from Helpdesk
+    """,
+
+    'author': "HAKTechnologies",
+    'website': "http://www.haktechnologies.com",
+    'images': ['static/description/icon.png'],
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'sale',
+    'version': '0.1',
+    'license': 'OPL-1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale', 'stock','helpdesk', 'repair', 'helpdesk_sale_timesheet','helpdesk_repair'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/helpdesk.xml',
+        'views/repair.xml',
+        # 'views/report_delivery_order.xml',
+        'wizard/repair_to_rfq_wiz_view.xml',
+        'wizard/repair_to_delivery_wiz_view.xml',
+        "data/sequencer.xml"
+    ],
+
+}
