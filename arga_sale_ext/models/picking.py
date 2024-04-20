f6f9c350527aab8f7f150bbd7a6e762e572927b8
Hunain AK <hunainfast@gmail.com>
2024-04-20 19:20:32 +0500

    Add files via upload

---
+++ arga_sale_ext/models/picking.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+class StockPickingInh(models.Model):
+    _inherit = 'stock.picking'
+
+    delivery_date = fields.Date(string='Delivery Date', copy=False)
+
+    def write(self, vals):
+        res = super(StockPickingInh, self).write(vals)
+        if vals.get('delivery_date'):
+            sale_order = self.env['sale.order'].search([("name", '=', self.origin)], limit=1)
+            if sale_order and sale_order.commitment_date != self.delivery_date:
+                sale_order.commitment_date = self.delivery_date
+        return res
---
+++ arga_sale_ext/models/project.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class ProjectTaskInh(models.Model):
+    _inherit = 'project.task'
+
+    delivery_date = fields.Date(string='Delivery Date', copy=False)
+
+    def write(self, vals):
+        res = super(ProjectTaskInh, self).write(vals)
+        if vals.get('delivery_date'):
+            if self.sale_line_id.order_id.commitment_date != self.delivery_date:
+                self.sale_line_id.order_id.commitment_date = self.delivery_date
