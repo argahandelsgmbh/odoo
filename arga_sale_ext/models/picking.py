321eb710570d7e02f6ad1ee08e3d100597108ed8
Hunain AK <hunainfast@gmail.com>
2024-04-30 17:36:03 +0500

    Update picking.py

--- arga_sale_ext/models/picking.py
+++ arga_sale_ext/models/picking.py
@@ -6,10 +6,10 @@ class StockPickingInh(models.Model):
 
     delivery_date = fields.Date(string='Delivery Date', copy=False)
 
-    def write(self, vals):
-        res = super(StockPickingInh, self).write(vals)
-        if vals.get('delivery_date'):
-            sale_order = self.env['sale.order'].search([("name", '=', self.origin)], limit=1)
-            if sale_order and sale_order.commitment_date != self.delivery_date:
-                sale_order.commitment_date = self.delivery_date
-        return res
+    # def write(self, vals):
+    #     res = super(StockPickingInh, self).write(vals)
+    #     if vals.get('delivery_date'):
+    #         sale_order = self.env['sale.order'].search([("name", '=', self.origin)], limit=1)
+    #         if sale_order and sale_order.commitment_date != self.delivery_date:
+    #             sale_order.commitment_date = self.delivery_date
+    #     return res
