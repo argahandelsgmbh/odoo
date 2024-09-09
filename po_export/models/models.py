b0aa435e798c2fa131eb2037fada398f0f0227cb
Hunain AK <hunainfast@gmail.com>
2024-09-09 14:43:01 +0500

    Add files via upload

---
+++ po_export/models/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+# class PurchaseOrder(models.Model):
+#     _inherit = 'purchase.order'
+#
+#     def generate_report(self):
+#         print(self)
+
+#         return self.env.ref('po_export.po_export_report_action').report_action(self,data='abc')
