93c55f438296b635bf316d58d65ec2dc67dab43b
Hunain AK <hunainfast@gmail.com>
2025-04-04 16:59:42 +0500

    Update pricelists.py

--- hak_products_var_import/models/pricelists.py
+++ hak_products_var_import/models/pricelists.py
@@ -33,8 +33,9 @@ class ProductVarImport(models.Model):
 
     def action_import_products(self):
         pcount = 0
-        ids=self.env.context.get('active_id')
-        pro_list=self.env['pricelist.pricelist'].search([("id","in",ids)])
+        model = self.env.context.get('active_model')
+        docs = self.env[model].browse(self.env.context.get('active_ids', []))
+        pro_list=self.env['pricelist.pricelist'].search([("id","in",docs)])
         _logger.info('ids %s', ids)
         for rec in pro_list:
             pcount = pcount + 1
