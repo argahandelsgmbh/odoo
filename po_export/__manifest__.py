e521729d9a42a07f5be2950b1fb6299f1b33e9d5
Hunain AK <hunainfast@gmail.com>
2026-02-14 17:01:32 +0500

    Update __manifest__.py

--- po_export/__manifest__.py
+++ po_export/__manifest__.py
@@ -23,9 +23,8 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
-        # 'reports/po_export.xml',
-        'views/views.xml',
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
 
     ],
 
