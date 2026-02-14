b46a47ad71d56b6b0667ccc4c16655d5190f0834
Hunain AK <hunainfast@gmail.com>
2026-02-14 16:26:20 +0500

    Add files via upload

--- exim_bellona/__manifest__.py
+++ exim_bellona/__manifest__.py
@@ -30,7 +30,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '19.4',
+    'version': '19.3',
 
     # any module necessary for this one to work correctly
     'depends': ['stock','sale','purchase'],
