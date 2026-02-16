7897fdd07a3b1eea152f6282094d1831af6530e7
Hunain AK <hunainfast@gmail.com>
2026-02-16 16:09:00 +0500

    Delete exim_bellona directory

--- exim_bellona/models/log_notes.py
+++
@@ -1,9 +0,0 @@
-from odoo import _, api, fields, models
-
-class BellonaLogNotes(models.Model):
-    _name = 'bellona.log.notes'
-    _description = "Bellona Logs"
-    _rec_name = 'error'
-
-    error = fields.Text('Error')
-    company_id = fields.Many2one('res.company', string='Company')
