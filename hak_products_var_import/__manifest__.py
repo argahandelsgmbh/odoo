70ca001ea39211458213f2f0e9ef4a77077198aa
Hunain AK <hunainfast@gmail.com>
2026-02-18 20:56:53 +0500

    Update __manifest__.py

--- hak_products_var_import/__manifest__.py
+++ hak_products_var_import/__manifest__.py
@@ -18,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '19.6',
+    'version': '19.1',
         'installable': True,
 
     # any module necessary for this one to work correctly
