76fdb2d2b75d9ed444f54a1909a4ebb3181ae3d2
Hunain AK <hunainfast@gmail.com>
2026-02-18 20:57:24 +0500

    Update __manifest__.py

--- payment_from_so/__manifest__.py
+++ payment_from_so/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '19.5',
+    'version': '19.1',
     'license': 'OPL-1',
         'installable': True,
 
