4f37b67d1b5360262b3789fffa5737fc858cefbc
Hunain AK <hunainfast@gmail.com>
2026-02-18 11:54:57 +0500

    Update __manifest__.py

--- website_floating_whatsapp_icon/__manifest__.py
+++ website_floating_whatsapp_icon/__manifest__.py
@@ -1,27 +1,8 @@
 # -*- coding: utf-8 -*-
-#############################################################################
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#
-#    Copyright (C) 2025-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
-#    Author: Cybrosys Techno Solutions (odoo@cybrosys.com>)
-#
-#    You can modify it under the terms of the GNU LESSER
-#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
-#
-#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
-#    (LGPL v3) along with this program.
-#    If not, see <http://www.gnu.org/licenses/>.
-#
-#############################################################################
+
 {
     'name': "WhatsApp Floating Icon in Website",
-    'version': '19.6',
+    'version': '19.7',
     'category': 'Extra Tools',
     'summary': """Adds a WhatsApp Floating Icon in Website to connect with
     WhatsApp web.""",
