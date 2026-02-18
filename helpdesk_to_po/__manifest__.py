e957151cd716c1ba38ede2f227cc8dded1c64ede
Hunain AK <hunainfast@gmail.com>
2026-02-18 22:55:43 +0500

    Add files via upload

--- helpdesk_to_po/__manifest__.py
+++ helpdesk_to_po/__manifest__.py
@@ -16,8 +16,10 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.2',
+    'version': '19.6',
      'license': 'OPL-1',
+     
+         'installable': True,
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'helpdesk', 'purchase','helpdesk_timesheet', 'helpdesk_stock'],
