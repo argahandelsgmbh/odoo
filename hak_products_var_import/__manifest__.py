42992b21ded4a01395264f46cf82716c141d1457
Hunain AK <hunainfast@gmail.com>
2026-02-18 22:30:52 +0500

    Add files via upload

--- hak_products_var_import/__manifest__.py
+++ hak_products_var_import/__manifest__.py
@@ -18,7 +18,8 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.5',
+    'version': '19.6',
+        'installable': True,
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'stock','product','sale'],
