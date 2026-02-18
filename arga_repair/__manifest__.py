ca04cabb88b38d2d6f2cae95bc0685aabc14bfa2
Hunain AK <hunainfast@gmail.com>
2026-02-18 20:56:28 +0500

    Update __manifest__.py

--- arga_repair/__manifest__.py
+++ arga_repair/__manifest__.py
@@ -19,7 +19,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'sale',
-    'version': '19.6',
+    'version': '19.1',
     'license': 'OPL-1',
         'installable': True,
 
