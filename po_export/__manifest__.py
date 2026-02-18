b05628da75f28071b69df14121a2c400c1b274d8
Hunain AK <hunainfast@gmail.com>
2026-02-18 11:59:33 +0500

    Add files via upload

---
+++ po_export/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "PO Export",
+
+    'summary': """
+        PO Export""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'license': 'OPL-1',
+    'version': '19.3',
+        'installable': True,
+
+    # any module necessary for this one to work correctly
+    'depends': ['base',  'purchase'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+
+    ],
+
+}
