f94256af9897518578aa6d371c5946bbe778559b
Hunain AK <hunainfast@gmail.com>
2026-04-05 22:07:07 +0500

    Update controllers.py

--- hak_archive_order/controllers/controllers.py
+++ hak_archive_order/controllers/controllers.py
@@ -4,12 +4,12 @@ from odoo.http import route
 
 class PaymentPortalInh(PaymentPortal):
 
-    @route()
-    def shop_payment_transaction(self, order_id, access_token, **kwargs):
-        order_sudo = self._document_check_access(
-            'sale.order', order_id, access_token
-        )
-        order_sudo.write({'active': True})
-        return super().shop_payment_transaction(order_id, access_token, **kwargs)
+    # @route()
+    # def shop_payment_transaction(self, order_id, access_token, **kwargs):
+    #     order_sudo = self._document_check_access(
+    #         'sale.order', order_id, access_token
+    #     )
+    #     order_sudo.write({'active': True})
+    #     return super().shop_payment_transaction(order_id, access_token, **kwargs)
 
 
