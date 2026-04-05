d5075d9b7d0733a31d4e8a93a840fd77e3ea7f41
Hunain AK <hunainfast@gmail.com>
2026-04-05 22:11:15 +0500

    Update controllers.py

--- hak_archive_order/controllers/controllers.py
+++ hak_archive_order/controllers/controllers.py
@@ -2,7 +2,7 @@
 from odoo.addons.payment.controllers.portal import PaymentPortal
 from odoo.http import route
 
-class PaymentPortalInh(PaymentPortal):
+# class PaymentPortalInh(PaymentPortal):
 
     # @route()
     # def shop_payment_transaction(self, order_id, access_token, **kwargs):
