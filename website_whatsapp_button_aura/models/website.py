90fcef9fba70deafe8cc2cdf39f3e57619df7c76
Hunain AK <hunainfast@gmail.com>
2026-05-13 17:17:12 +0500

    Update website.py

--- website_whatsapp_button_aura/models/website.py
+++ website_whatsapp_button_aura/models/website.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
 
-class Website(models.Model):
+class WebsiteWhatsapp(models.Model):
     _inherit = 'whatsapp.message'
 
     send_to = fields.Char(string="Sent")
